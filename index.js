65955985b9f2342d9a3d94140ac588d83f988775
Andrey606 <andreykuluev96@gmail.com>
2020-11-30 17:32:35 +0200

    sds

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
-const ZigbeeHerdsman = require('zigbee-herdsman');
-const zigbeeHerdsmanConverters = require('zigbee-herdsman-converters');
-
 console.log('Starting zigbeeAdapter v1.0.0' +'\n');
+
+// #1 input data
+const test = {
+    cluster: 6,
+    attribute: 0,
+    data: 0,
+    mac: 0x10,
+    modelId: 'lumi.plug',
+    endpoint: 1
+}
+console.log('test input data:');
+console.log(test);
+console.log('\n');
+
+// #2 find device object with modelId
+const zigbeeHerdsmanConverters = require('zigbee-herdsman-converters');
+const device = zigbeeHerdsmanConverters.findByZigbeeModel(test.modelId);
+console.log(device);
+console.log('\n');
+
+// #3 get commandId 
+const ZigbeeHerdsman = require('zigbee-herdsman');
+const clusterStruct = ZigbeeHerdsman.Zcl.Utils.getCluster(6)
+console.log(clusterStruct);
+console.log('\n');
+console.log(clusterStruct.name); // 'genOnOff'
+console.log('\n');
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // get data about some cluster from ZigbeeHerdsman
-let onOffCluster = ZigbeeHerdsman.Zcl.Utils.getCluster(6)
+//let onOffCluster = ZigbeeHerdsman.Zcl.Utils.getCluster(6)
 //console.log(onOffCluster)
 
 // zigbeeHerdsmanConverters 
@@ -21,7 +58,8 @@ let onOffCluster = ZigbeeHerdsman.Zcl.Utils.getCluster(6)
 
 
 
-const ExtensionReceive = require('zigbee2mqtt/lib/extension/receive');
-const recieve = new ExtensionReceive();
+//const ExtensionReceive = require('zigbee2mqtt/lib/extension/receive');
+//const recieve = new ExtensionReceive();
 
-console.log(recieve)
+//console.log(recieve)
+
