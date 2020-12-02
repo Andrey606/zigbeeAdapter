ad391687e8ed61ac26ce4d138f18317ccab357db
Andrey606 <andreykuluev96@gmail.com>
2020-12-02 17:12:16 +0200

    cvcx

--- index.js
+++ index.js
@@ -27,10 +27,96 @@ console.log('\n');
 console.log(clusterStruct.name); // 'genOnOff'
 console.log('\n');
 
+// #4 find fromZigbee converter 'genOnOff' from device Object
+let converter = null;
+device.fromZigbee.forEach( // перебираем все элементы массива array
+    function print( element ) {
+        if(element.cluster === clusterStruct.name)
+        {
+            console.log(element);
+            console.log('\n');
+            converter = element.convert;
+        }
+    }
+);
+
+// #5 используем нужный метод конвертера и заганяем в него нужные значения
+/**
+ * Documentation of convert() parameters
+ * - model: zigbee-herdsman-converters definition (form devices.js)
+ * - msg: message data property
+ * - publish: publish method
+ * - options: converter options object, e.g. {occupancy_timeout: 120}
+ * - meta: object containing {device: (zigbee-herdsman device object)}
+ */
+let payload = {};
+
+// model
+const model = device;
+// publish
+const publish = (payload) => {
+    console.log('publish: ' + payload + "!!!");
+    console.log('\n');
+}
+// resolvedEntity
+//const Zigbee = require('zigbee2mqtt/lib/zigbee');
+//let zigbee = new Zigbee();
+const Controller = require('zigbee2mqtt/lib/controller');
+const controller = new Controller();
+// data
+// 1) нужно заполнить структуру dataPayload
+const device = typeof dataPayload.address === 'string' ?
+                model_1.Device.byIeeeAddr(dataPayload.address) : model_1.Device.byNetworkAddress(dataPayload.address);
+// 2) заполнить eventData который и является data
+const eventData = {
+    //type: type, device, endpoint, data, linkquality, groupID, cluster: clusterName, meta
+};
+
+
+
+//controller.onZigbeeEvent('message', );
+
+//const resolvedEntity = zigbee.resolveEntity(data.device || data.ieeeAddr);
+// где берется data? => zclData -> ZclFrame структура, кроме них еще бывает rawData ...
+/* to do */
+
+// const converted =  converter(null, null);
 
 
 
 
+
+console.log(payload); // {state: 'OFF'}
+console.log('\n');
+
+const responseFrame = ZigbeeHerdsman.Zcl.ZclFrame.create(
+    ZigbeeHerdsman.Zcl.FrameType.GLOBAL, 
+    ZigbeeHerdsman.Zcl.Direction.SERVER_TO_CLIENT, 
+    true, 
+    null, 
+    100, 
+    'readRsp', 
+    6, 
+    [{attrId: 0, attrData: 0, dataType: 16, status: 0}]
+);
+console.log(responseFrame);
+
+
+
+
+const mockAdapterEvents = {};
+
+
+
+
+mockAdapterEvents['zclData'] = {
+    address: 129,
+    frame: ZigbeeHerdsman.Zcl.ZclFrame.create(0, 0, true, null, 40, 0, 10, [{attrId: 0}, {attrId: 1}, {attrId: 7}, {attrId: 9}]),
+    endpoint: 1,
+    linkquality: 19,
+    groupID: 10,
+};
+console.log(mockAdapterEvents);
 
 
 
